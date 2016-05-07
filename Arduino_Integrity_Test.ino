2380e317c7edc668ad94ef96ed6667f40eade909
Wyatt Johnson <wyattjohnson35@gmail.com>
2016-05-07 16:53:27 -0400

    Update 5.7.16.1
    I'm uploading this file because I used it to test the integrity of the Arduino to see if it still works. It has a lot of (lazy) functions that are used to decrease the amount of functional code used.

---
+++ Arduino_Integrity_Test.ino
@@ -0,0 +1,53 @@
+int led = 13;
+
+void tab(int tabNumber) { //tabs the Serial Monitor by tabNumber space(s)
+  for (int tabStart = 0; tabStart < tabNumber; tabStart++) {
+    Serial.println("");
+
+  }
+}
+
+void wait(int seconds) { //changes delay(milliseconds) to wait(seconds) to save time
+  delay(seconds * 1000);
+
+}
+
+void pinHigh(int pin) { //automatically sets pin to HIGH
+  digitalWrite(pin, HIGH);
+}
+
+void pinLow(int pin) { //automatically sets pin to LOW
+  digitalWrite(pin, LOW);
+}
+
+void Blink(int pin, int interval){
+  pinHigh(pin);
+  wait(interval);
+  pinHigh(pin);
+  wait(interval);
+  
+}
+void setup() {
+
+  Serial.begin(9600); //initialize Serial Communication at 9600 baud
+
+  Serial.println("Hello User");
+  tab(1);
+  wait(1);
+  Serial.println("Current Board is Arduino Uno");
+  //Pinmode Setup Code
+
+  pinMode(led, OUTPUT);
+
+
+  // put your setup code here, to run once:
+  for (int test = 0; test < 20; test++) {
+    Blink(led, 500);
+  }
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
